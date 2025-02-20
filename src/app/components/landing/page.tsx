9240e810c3486f2dc4ec1380a0c7935ae81302d5
Priyansh Patel <priyanshspatel2906@gmail.com>
2025-02-20 22:10:04 +0530

    light mode done

--- src/app/components/landing/page.tsx
+++ src/app/components/landing/page.tsx
@@ -3,26 +3,23 @@ import * as React from "react";
 import { useTheme } from "next-themes";
 import { Icons } from "../../components/iconProvider/page";
 import Image from "next/image";
+import styles from "./Landing.module.css";
 
 export default function Landing() {
   const { setTheme } = useTheme();
   return (
     <>
-      <div className="flex justify-between absolute w-full h-full">
+      <div className={styles.container}>
         {/* Portfolio owner name */}
-        <div className="overflow-hidden flex items-center">
-          <h1 className="pl-20 text-4xl sm:text-5xl md:text-6xl lg:text-9xl font-bold tracking-tighter text-transparent dark:text-transparent dark:[-webkit-text-stroke:1px_white] [-webkit-text-stroke:1px_black]">
+        <div className={styles.nameContainer}>
+          <h1 className={styles.name}>
             PRIYANSH
           </h1>
         </div>
 
-        {/* Oribiting techs */}
-        <div className="relative flex w-1/2 flex-col items-center justify-center rounded-lg">
-          {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
-        Circles
-      </span> */}
-          {/* <div className="flex justify-center items-center h-screen"> */}
-          <div className="w-40 h-40 md:w-60 md:h-60 lg:w-80 lg:h-80 rounded-full overflow-hidden border-4 border-white shadow-lg">
+        {/* Orbiting techs */}
+        <div className={styles.techContainer}>
+          <div className={styles.profileImage}>
             <Image
               src="/images/priyansh_profile.png"
               alt="profile-pic"
@@ -32,7 +29,6 @@ export default function Landing() {
               unoptimized
             />
           </div>
-          {/* </div> */}
           <OrbitingCircles iconSize={40} radius={225} speed={2}>
             <Icons.html />
             <Icons.css3 />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { useTheme } from "next-themes";
 import Navbar from "./components/navbar/page";
 import Landing from "./components/landing/page";
+import About from "./components/about/page";
 
 export default function Home() {
   const getPositionStyles = (index: number): React.CSSProperties => {
@@ -22,7 +23,7 @@ export default function Home() {
       <div className="absolute top-0 z-[-2] h-screen w-screen overflow-hidden light:bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] dark:bg-neutral-950">
         <Navbar />
         <Landing />
-        
+        <About/>
       </div>
     </>
   );
